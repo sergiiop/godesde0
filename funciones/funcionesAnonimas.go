62c60f433b26b2c463307b9fb79af2e05f802506
Sergio Perez <sergio-luis98@hotmail.com>
2024-02-06 15:01:31 -0500

    Siguiente commit

--- funciones/funcionesAnonimas.go
+++ funciones/funcionesAnonimas.go
@@ -3,9 +3,19 @@ package funciones
 import "fmt"
 
 func Calculos() {
-	suma := func(number1 int, number2 int) int {
-		return number1 + number2
+
+	var numero3 int = 21
+	var numero4 int = 221
+
+	calculo := func(number1 int, number2 int) int {
+		return number1 + number2 + numero3
+	}
+	fmt.Println(calculo(10, 25))
+
+
+	calculo = func(number1 int, number2 int) int {
+		return number1 + number2 + numero3 + numero4
 	}
 
-	fmt.Println(suma(10, 25))
+	fmt.Println(calculo(10, 25))
 }
